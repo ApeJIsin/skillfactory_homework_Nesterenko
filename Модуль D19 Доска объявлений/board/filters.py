fbed3c496e9e020ff3eac071b74a24c4404196dc
Nesterenko_Anton <ofigel009@mail.ru>
2024-07-09 22:44:05 +0500

    Добавление задания по модулю D19

---
+++ Модуль D19 Доска объявлений/board/filters.py
@@ -0,0 +1,16 @@
+from django_filters import FilterSet
+
+from .models import Reply, Post
+
+
+class PostFilter(FilterSet):
+
+    class Meta:
+        model = Reply
+        fields = [
+            'post'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(PostFilter, self).__init__(*args, **kwargs)
+        self.filters['post'].queryset = Post.objects.filter(author_id=kwargs['request'])
